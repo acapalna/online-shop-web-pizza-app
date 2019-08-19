b10845674cfa0932e64c0977bc7e8dcc42d05652
augustin.capalna <augustin.capalna@yahoo.com>
2019-08-20 00:26:06 +0300

    First try of display single product

---
+++ js/cart.js
@@ -0,0 +1,60 @@
+const API_URL = "http://localhost:8090";
+
+window.Cart = {
+
+    getCart:function(){
+        let clientId = 7;
+
+        $.ajax({
+            url: API_URL + "/cart/" + clientId,
+            method: "GET",
+
+        }).done(function(response) {
+            console.log(response);
+            Cart.displayProducts(response.pizza)
+        });
+    },
+
+    getProductHtml:function(product){
+    return `<tr class="cart_item">
+                <td class="product-remove">
+                    <a title="Remove this item" class="remove" href="#">×</a>
+            </td>
+    
+            <td class="product-thumbnail">
+                <a href="single-product.html"><img width="145" height="145" alt="poster_1_up" class="shop_thumbnail" src=${product.imagePath}></a>
+                </td>
+    
+                <td class="product-name">
+                <a href="single-product.html">${product.name}</a>
+            </td>
+    
+            <td class="product-price">
+                <span class="amount">£${product.price}</span>
+            </td>
+    
+            <td class="product-quantity">
+                <div class="quantity buttons_added">
+                <input type="button" class="minus" value="-">
+                <input type="number" size="4" class="input-text qty text" title="Qty" value="1" min="0" step="1">
+                <input type="button" class="plus" value="+">
+                </div>
+                </td>
+    
+                <td class="product-subtotal">
+                <span class="amount">£${product.price}</span>
+            </td>
+            </tr>`
+    },
+
+    displayProducts: function (products) {
+        let productsHtml = "";
+        products.forEach(item => productsHtml += Cart.getProductHtml(item));
+
+        //cssSelector
+        $('.shop_table.cart tbody').html(productsHtml);
+    },
+
+};
+
+Cart.getCart();
---
+++ js/menu.js
@@ -0,0 +1,101 @@
+const API_URL = "http://localhost:8090";
+
+window.Shop = {
+
+    addProductToCart: function (productId){
+        let body = {
+            clientId: 7,
+            pizzaId: productId,
+        };
+
+        $.ajax({
+            url:API_URL + "/cart",
+            method: "PUT",
+            //MIME type
+            contentType: "application/json",
+            data: JSON.stringify(body),
+        }).done(function () {
+            window.location.replace("cart.html")
+        });
+    },
+
+
+
+    getProducts:function(){
+        $.ajax({
+            url: API_URL + "/products",
+            method: "GET"
+        }).done(function(response) {
+            console.log(response);
+            Shop.displayProducts(response.content)
+        });
+    },
+
+    getProductHtml:function(product){
+
+        return `<div class="col-md-3 col-sm-6">
+                    <div class="single-shop-product">
+                        <div class="product-upper">
+                            <img src="${product.imagePath}" alt="">
+                        </div>
+                        <h2><a class="product-name" data-product_id="${product.id}" href="/canvas/menu/?add-to-cart=${product.id}">${product.name}</a></h2>
+                        <div class="product-carousel-price">
+                            <ins>$${product.price}</ins> <del>$${product.price}</del>
+                        </div>  
+                        
+                        <div class="product-option-shop">
+                            <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="${product.id}" rel="nofollow" href="/canvas/shop/?add-to-cart=${product.id}">Add to cart</a>
+                        </div>                       
+                    </div>
+                </div>`
+    },
+
+    displayProducts: function (products) {
+        let productsHtml = "";
+        products.forEach(item => productsHtml += Shop.getProductHtml(item));
+
+        //cssSelector
+        $('#products-container').html(productsHtml);
+    },
+
+    bindEvents: function () {
+        $('#products-container').delegate(
+            '.add_to_cart_button', 'click', function (event) {
+            event.preventDefault();
+
+            let productId = $(this).data('product_id');
+            Shop.addProductToCart(productId)
+        });
+    },
+
+
+    openProductInDetailPage: function (productId){
+        // let body = {
+        //     pizzaId: productId,
+        // };
+
+        $.ajax({
+            url:API_URL + "/products/" + productId,
+            method: "GET",
+            //MIME type
+            // contentType: "application/json",
+            // data: JSON.stringify(body),
+        }).done(function () {
+            window.location.replace("detail-page.html?id="+ productId)
+        });
+    },
+
+    openDetail: function () {
+        $('#products-container').delegate(
+            '.product-name', 'click', function (event) {
+                event.preventDefault();
+
+                let productId = $(this).data('product_id');
+                Shop.openProductInDetailPage(productId)
+            });
+    }
+};
+
+Shop.getProducts();
+Shop.bindEvents();
+Shop.openDetail();
